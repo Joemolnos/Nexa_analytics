b0c2c35e3cadc43e3c96c40e69838dd6df08b131
Molnos József <94288615+Joemolnos@users.noreply.github.com>
2025-04-16 13:29:53 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,6 +8,6 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # Import the Flask app from app.py
 from app import app
 
-# Vercel serverless function handler
-def handler(request):
-    return app(request.environ, lambda status, headers, exc_info=None: Response(None, status, headers))
+# WSGI handler
+def application(environ, start_response):
+    return app(environ, start_response)
